df2a625ff55c1b4b3e67edb96dfd39e219b9aff3
Dimitri AIGLE <56607329+dim-sDev@users.noreply.github.com>
2023-12-31 01:57:49 -0400

    Chapitre 10 Leçons 62

--- fullstack/api/src/index.js
+++ fullstack/api/src/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
 let count;
-// const uri = process.env.NODE_ENV === 'production' ?
-//     `mongodb://${ process.env.MONGO_USERNAME }:${ process.env.MONGO_PWD }@db` :
-//     `mongodb://db`;
+const uri =
+    process.env.NODE_ENV === 'production'
+        ? `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PWD}@db`
+        : `mongodb://db`;
 
-const uri = `mongodb://db`;
 const client = new MongoClient(uri);
 async function run() {
     try {
@@ -13,6 +13,9 @@ async function run() {
         await client.db('admin').command({ ping: 1 });
         console.log('CONNEXION DB OK !');
         count = client.db('test').collection('count');
+        if ((await count.countDocuments()) === 0) {
+            count.insertOne({ count: 0 });
+        }
     } catch (err) {
         console.log(err.stack);
     }
@@ -22,13 +25,19 @@ run().catch(console.dir);
 const app = express();
 
 app.get('/api/count', (req, res) => {
-    count.findOneAndUpdate({}, { $inc: { count: 1 } }, { returnNewDocument: true, upsert: true }).then((doc) => {
-        res.status(200).json(doc ? doc.count : 0);
-    })
-})
+    count
+        .findOneAndUpdate(
+            {},
+            { $inc: { count: 1 } },
+            { returnNewDocument: true, upsert: true }
+        )
+        .then((doc) => {
+            res.status(200).json(doc ? doc.count : 0);
+        });
+});
 
 app.all('*', (req, res) => {
     res.status(404).end();
-})
+});
 
 app.listen(80);
